75eff5951bb4aad1f1181923dc2cfe96f9a90fba
Sree Sai Raghava <raghava.rym@gmail.com>
2023-01-08 13:29:38 +0530

    Initial Commit

---
+++ RandomFacts/RandomFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomFactsApp.swift
+//  RandomFacts
+//
+//  Created by Sree Sai Raghava Dandu on 08/01/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
